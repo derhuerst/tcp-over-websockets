b56089ac23ffd00a016d2910ba60a5046451de1f
Jannis R <mail@jannisr.de>
2017-03-25 17:59:39 +0100

    server :sparkles:

--- server.js
+++ server.js
@@ -1,3 +1,36 @@
+#!/usr/bin/env node
 'use strict'
 
-// todo
+const net = require('net')
+const http = require('http')
+const url = require('url')
+const path = require('path')
+const ws = require('websocket-stream')
+
+const showError = (msg) => {
+	console.error(msg)
+	process.exit(1)
+}
+
+const validPort = /^[\d]+$/
+
+const verifyClient = ({req}, cb) => {
+	const port = url.parse(req.url).pathname.slice(1)
+	req.tunneledPort = +port
+	cb(validPort.test(port), 400, 'invalid port')
+}
+
+const httpServer = http.createServer()
+const wsServer = ws.createServer({
+	server: httpServer,
+	verifyClient
+}, (remote) => {
+	const req = remote.socket.upgradeReq
+	const local = net.createConnection({port: req.tunneledPort})
+	local.pipe(remote).pipe(local)
+})
+
+httpServer.listen(8080, (err) => {
+	if (err) showError(err)
+	else console.info('listening on 8080')
+})
